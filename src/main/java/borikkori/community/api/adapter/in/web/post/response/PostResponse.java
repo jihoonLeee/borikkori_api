1e6d466f857305d393ef8d4e3694bd16e4925784
jihoonLeee <jihoon2723@naver.com>
2025-05-18 14:11:53 +0900

    게시글 목록에 카테고리 추가

--- src/main/java/borikkori/community/api/adapter/in/web/post/response/PostResponse.java
+++ src/main/java/borikkori/community/api/adapter/in/web/post/response/PostResponse.java
@@ -2,6 +2,7 @@ package borikkori.community.api.adapter.in.web.post.response;
 
 import java.time.LocalDateTime;
 
+import borikkori.community.api.domain.post.entity.Category;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,6 +22,8 @@ public class PostResponse {
 	private int visitCnt;
 
 	private int likeCnt;
+
+	private Category category;
 
 	@Setter
 	private boolean isTemp = false; // 임시 게시물 용
